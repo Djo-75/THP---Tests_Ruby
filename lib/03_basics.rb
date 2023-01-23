fea87eeab5c6f3795c4928e479943e0d60835f92
Djo_75 <119299504+Djo-75@users.noreply.github.com>
2023-01-24 00:54:55 +0100

    Add files via upload

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,30 +1,28 @@
-# def who_is_bigger(a, b, c)
-#     if [a, b, c].include?(nil)
-#         return "nil detected"
-#     elsif a > b && a > c
-#         return "a is bigger"
-#     elsif b > a && b > c
-#         return "b is bigger"
-#     elsif c > a && c > b
-#         return "c is bigger"
-#   end
-# end
-
-# puts who_is_bigger(21, 0, 6373)
-
-# def reverse_upcase_noLTA(text)
-#   result1 = text.reverse
-#   result2 = result1.upcase
-#   result3 = result2.delete('LTA')
-# end
-
-# puts reverse_upcase_noLTA("Damienlta")
-
-def array_42([1,3])
-  if array.include?(42) = true
-
+def who_is_bigger(a, b, c)
+    if [a, b, c].include?(nil)
+        return "nil detected"
+    elsif a > b && a > c
+        return "a is bigger"
+    elsif b > a && b > c
+        return "b is bigger"
+    elsif c > a && c > b
+        return "c is bigger"
+  end
 end
 
-# def 'crazy stuff on arrays'
 
-# end
+def reverse_upcase_noLTA(text)
+  result1 = text.reverse
+  result2 = result1.upcase
+  result3 = result2.delete('LTA')
+end
+
+
+def array_42(array)
+  array.include? 42
+end
+
+def magic_array(array)
+  (((array.flatten).sort.map{|a| a * 2}.delete_if{|n| n%3 == 0}).uniq)
+end
+
---
+++ lib/04_simon_says.rb
@@ -0,0 +1,20 @@
+def echo
+  return "hello"
+end
+
+def echo
+  return "bye"
+end
+
+def shout(a)
+    text = a.upcase
+return text
+end
+
+
+def shout(a, b)
+  text = (a + " " + b).upcase
+return text
+end
+
+
